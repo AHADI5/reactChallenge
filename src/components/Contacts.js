4f9cb709c8b199665394c6d173175bb5501e1191
AHADI5 <gloireahadi9@gmail.com>
2024-02-10 20:38:57 +0100

    business Card completed

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,13 +1,14 @@
 import Facebook from "../images/facebook.svg";
 import Instagram from "../images/instagram.svg";
 import Whatsapp from "../images/whatsapp.svg";
-
+import Twitter from "../images/twitterx.svg"
 export default function Contacts () {
     return (
         <div className="contacts">
             <img src={Facebook} alt="facebook"/>
             <img src={Instagram} alt="instagram"/>
             <img src={Whatsapp} alt="whatsaap"/>
+            <img src={Twitter} alt="twitter"/>
         </div>
     )
 
